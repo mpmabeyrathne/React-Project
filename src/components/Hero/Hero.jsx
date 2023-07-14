077cf405654de8e853dd5cbb18a63091dd3dd289
MPM Abeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-14 19:47:49 +0530

    Hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth hero-container flexCenter">
         {/* Left side */}
-        <div className="hero-left">
+        <div className="flexColStart hero-left">
           <div className="hero-title">
+            <div className="orange-circle" />
             <h1>
               Discover <br /> Most Suitable <br /> Property
             </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span>
+              Find a varienty of properties that suit you very easilty
+            </span>
+            <span>
+              Forget all difficulties in finding a Residencies for you
+            </span>
+          </div>
+          <div className="search-bar flexCenter">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
           </div>
         </div>
         {/* Right side */}
