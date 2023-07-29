03b9299f43e0839a9bf700a7c6c674537b830f94
MPM Abeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-29 09:48:01 +0530

    companies section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Header></Header>
         <Hero></Hero>
       </div>
+      <Companies />
     </div>
   );
 }
